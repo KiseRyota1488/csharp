8787597a6553ef484c4df5df0661190454488d60
KiseRyota1488 <shavaryn_ak19@nuwm.edu.ua>
2021-04-03 21:12:28 +0300

    hw4

---
+++ Homeworks/Homework 4/AllClasses/Classes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AllClasses
+{
+    public abstract class Unit
+    {
+        public int Hp { get; set; }
+        public int Damage { get; set; }
+        public int EvadeChance { get; set; }
+
+        public Unit()
+        {
+            
+        }
+
+    }
+
+    class Swordman : Unit
+    {
+        public Swordman()
+        {
+            Hp = 15;
+            Damage = 5;
+            EvadeChance = 30;
+        }
+    }
+    class Archer : Unit
+    {
+        public Archer()
+        {
+            Hp = 12;
+            Damage = 4;
+            EvadeChance = 60;
+        }
+    }
+    class Mage : Unit
+    {
+        public Mage()
+        {
+            Hp = 8;
+            Damage = 10;
+            EvadeChance = 40;
+        }
+    }
+    
+    
+    public class Team
+    {
+        public Unit Unit { get; set; }
+        public Team()
+        {
+            Random rnd = new Random();
+            int choice = rnd.Next(3);
+
+            switch (choice)
+            {
+                case 0:
+                    Unit = new Swordman();
+                    break;
+                case 1:
+                    Unit = new Archer();
+                    break;
+                case 2:
+                    Unit = new Mage();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+}
---
+++ Homeworks/Homework 4/PlaceOfBattle/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using AllClasses;
+using System.Collections.Generic;
+
+
+namespace PlaceOfBattle
+{
+    enum Classes
+    {
+        Swordman = 1,
+        Archer,
+        Mage
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int roundCounter = 0;
+            List<Team> alliance = new List<Team>() { new Team(), new Team(), new Team() };
+            List<Team> orcs = new List<Team>() { new Team(), new Team(), new Team() };
+
+            
+            OneRound(ref alliance, ref orcs, ref roundCounter);
+        }
+        static void OneRound(ref List<Team> alliance, ref List<Team> orcs,ref int roundCounter)
+        {
+            Random rnd = new Random();
+            int choice = rnd.Next(3);
+
+            if (roundCounter % 2 == 0)
+            {
+                ChooseEnemy(alliance[choice].Unit.GetType().Name, ref orcs).Unit.Hp -= alliance[choice].Unit.Damage;
+                
+            }
+
+
+
+            roundCounter++;
+        }
+
+        static Team ChooseEnemy(string classType, ref List <Team> team)
+        {
+            int it = 0;
+            foreach (var item in team)
+            {
+                if (item.Unit.GetType().Name == classType)
+                {
+                    return item;
+                }
+                else if(it == 2)
+                    return item;
+                it++;
+            }
+
+            return new Team();
+        }
+    }
+
+
+
+}
