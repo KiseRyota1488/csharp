7432094cf367a81d4ffd91293560a5394d36bcd1
Til-tune <shavaryn_ak19@nuwm.edu.ua>
2021-04-11 13:23:42 +0300

    hw6

---
+++ Lessons/Lesson 6/T1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B d = new B() { J = 5 };
+            B f = new B() { J = 5 };
+
+
+            Console.WriteLine(d.GetHashCode());
+            Console.WriteLine(f.GetHashCode());
+        }
+
+    }
+
+    class B
+    {
+        public int J { get; set; }
+    }
+    
+}
