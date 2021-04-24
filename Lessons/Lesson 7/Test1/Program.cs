7d261bc4f3965a539a41b81605ea97744ba65941
Til-tune <shavaryn_ak19@nuwm.edu.ua>
2021-04-24 15:26:37 +0300

    les7

---
+++ Lessons/Lesson 7/Test1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test1
+{
+    delegate void M(string s);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            M v = hell;
+            v += hell;
+            
+        }
+        static void hell(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
