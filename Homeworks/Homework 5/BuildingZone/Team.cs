98a3ead1580cd3bd9fb265d02454d84c7f852151
Til-tune <shavaryn_ak19@nuwm.edu.ua>
2021-04-25 13:26:10 +0300

    les8

--- Homeworks/Homework 5/BuildingZone/Team.cs
+++ Homeworks/Homework 5/BuildingZone/Team.cs
@@ -11,19 +11,7 @@ namespace BuildingZone
 
         public void Building()
         {
-            teamLeader.lastPart = new Basement().GetType();
-
-            for (int i = 0; i < 11; i++)
-            {
-                if (i == 0)
-                    
-
-                worker.currentPart = teamLeader.lastPart;
-
-                teamLeader.Working();
-                worker.Working();
-
-            }
+            
         }
 
         void DrawCurrentStage()
